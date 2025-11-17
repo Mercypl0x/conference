26932eff251b158e91dd7598a333a2696a98db61
Hagen Leitner <hagen.leitner@student.tugraz.at>
2025-11-17 11:01:37 +0100

    update registersw

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,22 +1,5 @@
-(function () {
-  function basePath(url) {
-    return url.pathname.substring(0, url.pathname.lastIndexOf("/"));
-  }
-
-  if ("serviceWorker" in navigator) {
-    const url = new URL(document.currentScript.src, self.location.href);
-    console.log(url.href, url.pathname, basePath(url));
-
-    const scope = `${basePath(url)}/`;
-    navigator.serviceWorker
-      .register(`${scope}sw.js`, { scope })
-      .then(function (registration) {
-      });
-
-    navigator.serviceWorker
-      .ready
-      .then(function (registration) {
-      });
-  }
-
-})();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/conference/sw.js", {
+    scope: "/conference/"
+  });
+}
