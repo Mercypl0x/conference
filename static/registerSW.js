05aa4a57a5a5241d5f4bbbe4c146a81fb565a14e
Hagen Leitner <hagen.leitner@student.tugraz.at>
2025-11-17 11:17:07 +0100

    manifest found now, update SW again

--- static/registerSW.js
+++ static/registerSW.js
@@ -1,5 +1,20 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/conference/sw.js", {
-    scope: "/conference/"
-  });
-}
+(function () {
+  function basePath(url) {
+    return url.pathname.substring(0, url.pathname.lastIndexOf("/"));
+  }
+
+  if ("serviceWorker" in navigator) {
+    const url = new URL(document.currentScript.src, self.location.href);
+    const scope = `${basePath(url)}/`;
+    navigator.serviceWorker
+      .register(`${scope}sw.js`, { scope })
+      .then(function (registration) {
+      });
+
+    navigator.serviceWorker
+      .ready
+      .then(function (registration) {
+      });
+  }
+
+})();
